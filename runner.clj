62db4aafc33f13b8debdc857d647b9d3ad5a2d5e
Pavlos Vinieratos <pvinis@gmail.com>
2022-05-13 19:43:56 +0100

    batman

---
+++ runner.clj
@@ -0,0 +1,42 @@
+#!/usr/bin/env bb
+
+
+(require '[babashka.fs :as fs])
+(require '[clojure.string :as string])
+
+(when (not= 1 (count *command-line-args*))
+  (println "Only use one arg, with the path to the multishcript you want to run.")
+  (System/exit 1))
+
+(def file (first *command-line-args*))
+
+(when (not (fs/exists? file))
+  (println "Multishcript file doesn't exist.")
+  (System/exit 1))
+
+(def lines (fs/read-all-lines file))
+
+(defn group-blocks
+  [acc next]
+
+  (case next
+    "```bash" (conj acc [next])
+    "```" (let [new-array (conj (last acc) next)]
+            (conj (vec (drop-last acc)) new-array))
+    (let [new-array (conj (last acc) next)]
+      (conj (vec (drop-last acc)) new-array))))
+
+;; (pprint (reduce group-blocks [] '("```bash" "wow" "```" "```bash" "yes" "```")))
+
+(->> lines
+     (remove #(= "" %))
+     (remove #(string/starts-with? % "#"))
+     (reduce group-blocks [])
+     pprint)
+
+;; (def codeblocks [])
+
+;; (with-open [rdr (open-file file)]
+;;   (println "ok"))
+
+;;;; checks? what if i dont close with ```, or if i detect an opening ```?
